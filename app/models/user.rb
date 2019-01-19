a61ec4570ffb2d354443cd14b7d4ab195ea42a6d
carly <carly@noukod.com>
2019-01-19 19:04:11 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true
   has_secure_password
-  validates :username,presence: true
-  validates :password,presence: true
   has_many :tweets
+
+  def self.find_by_slug(slug)
+    self.all.find{ |user| user.slug == slug }
+  end
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
 end

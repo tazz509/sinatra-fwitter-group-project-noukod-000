7a97751bb8cc3f57929a45e69e272ff1523b560a
carly <carly@noukod.com>
2019-01-19 19:12:10 +0000

    Automatically backed up by Learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,16 +6,16 @@
 #   it 'can slug the username' do
 #     expect(@user.slug).to eq("test-123")
 #   end
-# 
+#
 #   it 'can find a user based on the slug' do
 #     slug = @user.slug
 #     expect(User.find_by_slug(slug).username).to eq("test 123")
 #   end
-# 
+#
 #   it 'has a secure password' do
-# 
+#
 #     expect(@user.authenticate("dog")).to eq(false)
-# 
+#
 #     expect(@user.authenticate("test")).to eq(@user)
 #   end
 # end
